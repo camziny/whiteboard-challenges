9034e7c133833662a9f9201a24d6897da56d3ed5
camziny <cameronziny@gmail.com>
2022-08-05 13:46:43 -0400

    -array to BST

---
+++ convertArrayToTree/arrayToTree.js
@@ -0,0 +1,15 @@
+// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+// A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+const sortedArrayToBST = (nums, left = 0, right = nums.length - 1) => {
+  if (left > right) return null;
+
+  let mid = Math.floor((left + right) / 2);
+  let root = new TreeNode(nums[mid]);
+
+  root.left = sortedArrayToBST(nums, left, mid - 1);
+  root.right = sortedArrayToBST(nums, mid + 1, right);
+
+  return root;
+};
