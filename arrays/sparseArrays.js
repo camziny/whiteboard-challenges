03c3bda3ca9a2436a50463fd4d6a1a36294c06b7
camziny <cameronziny@gmail.com>
2022-08-29 12:24:34 -0400

    -sparseArrays

---
+++ arrays/sparseArrays.js
@@ -0,0 +1,11 @@
+// There is a collection of input strings and a collection of query strings. 
+// For each query string, determine how many times it occurs in the list of input strings. 
+// Return an array of the results.
+
+
+
+function matchingStrings(strings, queries) {
+    const stack = {};
+    for (const string of strings) stack[string] = (stack[string] || 0) + 1;
+    return queries.map((query) => stack[query] || 0);
+}
