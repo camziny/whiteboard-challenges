21d24f796d84378df59d010814f15dc416300a60
camziny <cameronziny@gmail.com>
2023-06-16 05:39:38 -0400

    -python prompt

---
+++ scalePrompts/pythinPrompts.py
@@ -0,0 +1,56 @@
+# Import the necessary libraries
+import datetime
+
+# Get the current date and time
+today = datetime.datetime.now()
+
+# Print the current date and time
+print(today)
+
+# Print the current day of the week
+print(today.weekday())
+
+# Print the current month
+print(today.month)
+
+# Print the current year
+print(today.year)
+
+# Print the current hour
+print(today.hour)
+
+# Print the current minute
+print(today.minute)
+
+# Print the current second
+print(today.second)
+
+# Get the current time
+now = datetime.datetime.now()
+
+# Print the current time
+print("The current time is:", now)
+
+# Print the date
+print("The current date is:", now.date())
+
+# Print the time
+print("The current time is:", now.time())
+
+# Print the year
+print("The current year is:", now.year)
+
+# Print the month
+print("The current month is:", now.month)
+
+# Print the day
+print("The current day is:", now.day)
+
+# Print the hour
+print("The current hour is:", now.hour)
+
+# Print the minute
+print("The current minute is:", now.minute)
+
+# Print the second
+print("The current second is:", now.second)
