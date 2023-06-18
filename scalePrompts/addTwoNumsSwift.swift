39f5b4651b706c4c4cbd0ecf0f5977b2a86c04c7
camziny <cameronziny@gmail.com>
2023-06-18 07:37:05 -0400

    -add two nums swift

---
+++ scalePrompts/addTwoNumsSwift.swift
@@ -0,0 +1,11 @@
+// Add Two Numbers program in Swift
+
+// Declare two variables to store the numbers
+var num1 = 4
+var num2 = 5
+
+// Add the two numbers and store the result in a variable
+var sum = num1 + num2
+
+// Print the result
+print("The sum of \(num1) and \(num2) is \(sum)")
