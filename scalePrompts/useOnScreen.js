5fbf05f09c4c354900e2cd936e999a2a0a92b590
camziny <cameronziny@gmail.com>
2023-06-23 10:21:13 -0400

    -use on screen

---
+++ scalePrompts/useOnScreen.js
@@ -0,0 +1,19 @@
+const useOnScreen = () => {
+  const [currentSection, setCurrentSection] = useState < string > "";
+  useEffect(() => {
+    function handleScroll() {
+      const sections = document.querySelectorAll("section");
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= sectionTop - sectionHeight / 3) {
+          setCurrentSection(section.getAttribute("id") ?? "");
+        }
+      });
+    }
+    window.addEventListener("scroll", handleScroll);
+    handleScroll(); // Remove the listener as soon as the component is unmounted
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrollY]);
+  return currentSection;
+};
