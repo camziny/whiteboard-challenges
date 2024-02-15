c259f080221afb37da4790a251e13915f326eeba
camziny <cameronziny@gmail.com>
2024-02-15 18:06:28 -0500

    - viral advertising

---
+++ hr/viralAdvertising.js
@@ -0,0 +1,13 @@
+function viralAdvertising(n) {
+  let shared = 5;
+  let liked = 0;
+  let cumulative = 0;
+
+  for (let i = 1; i <= n; i++) {
+    liked = Math.floor(shared / 2);
+    cumulative += liked;
+    shared = liked * 3;
+  }
+
+  return cumulative;
+}
